b4c6ac571616da601214a952e347e7c6739baa34
Melissa Villand <melissa.villand@bearingpoint.com>
2025-01-15 11:10:13 +0200

    Initial Commit

---
+++ Cinemate/CinemateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemateApp.swift
+//  Cinemate
+//
+//  Created by Melissa Villand on 15.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CinemateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cinemate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cinemate
+//
+//  Created by Melissa Villand on 15.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
