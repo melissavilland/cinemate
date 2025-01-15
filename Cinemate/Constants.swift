dd9956832a6ecf751326dd97fd9c5308909211c5
Melissa Villand <melissa.villand@bearingpoint.com>
2025-01-15 11:30:05 +0200

    first commit

---
+++ Cinemate/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Cinemate
+//
+//  Created by Melissa Villand on 15.01.2025.
+//
+
+import Foundation
+
+struct Constants {
+    static let homeString = "Home"
+    static let upcomingString = "Upcoming"
+    static let searchString = "Search"
+    static let downloadString = "Download"
+    
+    static let homeIconString = "house"
+    static let upcomingIconString = "play.circle"
+    static let searchIconString = "magnifyingglass"
+    static let downloadIconString = "arrow.down.to.line"
+
+}
---
+++ Cinemate/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Cinemate
+//
+//  Created by Melissa Villand on 15.01.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
